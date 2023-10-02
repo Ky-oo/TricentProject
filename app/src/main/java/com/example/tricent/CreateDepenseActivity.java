d7e4841498023bdf770ca83ecf6bcc84218fae7a
INZOUDINE.Hamlaoui <hamlaoui.inzoudine@lycee-faure.fr>
2023-10-02 11:50:23 +0200

    test commit new activity

---
+++ app/src/main/java/com/example/tricent/CreateDepenseActivity.java
@@ -0,0 +1,14 @@
+package com.example.tricent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateDepenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_depense);
+    }
+}
