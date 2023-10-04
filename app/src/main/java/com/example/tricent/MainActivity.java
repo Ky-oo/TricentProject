c02ff4e354450f527fa7023dc1b9e3e3954956ee
benmsatef.anas <anas.benmsatef@lycee-faure.fr>
2023-10-04 10:46:37 +0200

    création de redirection de bouton creer projet

--- app/src/main/java/com/example/tricent/MainActivity.java
+++ app/src/main/java/com/example/tricent/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.tricent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button_create_project);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, CreatProjetActivity.class);
+        startActivity(intent);
     }
 }
